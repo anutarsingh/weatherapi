9073172f0c887f632de093c3eab6b046bd46c708
anutarsingh <157003035+anutarsingh@users.noreply.github.com>
2024-02-15 14:37:56 +0530

    o updatre

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function fetchWeatherData(cityName){
 	let humidity = document.getElementById("humidity");
 	humidity.innerHTML = data.main.humidity+" g/kg";
 	let temp = document.getElementById("temp");
-	temp.innerHTML = Math.round((data.main.temp)-273.15)+"0°";
+	temp.innerHTML = Math.round((data.main.temp)-273.15)+"°";
 	console.log(data);
 }
 function fetchCity() {
